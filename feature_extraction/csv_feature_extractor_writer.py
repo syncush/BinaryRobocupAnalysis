4693fdc2258312258e26dadc575e3da433ad57a3
syncush <danielhermonsync@gmail.com>
2018-04-16 19:03:04 +0300

    Creating a CSV writer

--- feature_extraction/csv_feature_extractor_writer.py
+++ feature_extraction/csv_feature_extractor_writer.py
@@ -3,15 +3,33 @@ import csv
 from os import walk
 import extractor
 
-def write_to_csv(dir_to_analyze_path, dst_csv_path, csv_file_name):
+
+def str2bool(v):
+    return v.lower() in ("yes", "true", "t", "1")
+
+
+def write_to_csv(dir_to_analyze_path='/home/syncush/PycharmProjects/BinaryRobocupAnalysis/test', dst_csv_path='/home/syncush/Desktop', csv_file_name='BinaryRobocup', use_accurate=False):
     binaryPaths = []
     for (dirpath, dirnames, filenames) in walk(dir_to_analyze_path):
-        temp = [dirpath + x for x in filenames]
+        temp = [dirpath + '/' + x for x in filenames]
         binaryPaths.extend(temp)
-    with open(dst_csv_path + csv_file_name + ".csv", 'wb') as out:
+    with open(dst_csv_path + "/" + csv_file_name + ".csv", 'wb') as out:
         csv_out = csv.writer(out)
-        csv_out.writerow(['name', 'num'])
+        csv_out.writerow(["Binary Path", "cfg_node_size-sum", "cfg_node_size-max", "cfg_syscalls-count", "cfg_syscalls-names", "cfg_looping_time-sum", "cfg_looping_time-max", "cfg_return-count", "cfg_block_size- max", "radare2-max CC", "radare2-func_CC tuple", "complex CC", "simple CC"])
         for binaryPath in binaryPaths:
             analyzer = extractor.FeatureExtractor(binaryPath)
-            for row in data:
-                csv_out.writerow(row)
+            features = analyzer.analyze_everything(use_accurate)
+            cfg_node_size = [features[0]["sum"], features[0]["max"]]
+            cfg_sys_calls_info = [features[1]["sum"], '/'.join(features[1]["names"])]
+            cfg_looping_times_info = [features[2]["sum"], features[2]["max"]]
+            cfg_return_info = [features[3]["sum"]]
+            cfg_block_instructions_info = [features[4]["max"]]
+            radare2_max_complexity = [features[5]["max"]]
+            radare2_complexity = ['/'.join([x + '-' + str(r) for x, r in features[5]["list"]])]
+            angr_mc_cabe_complexity = [features[6], features[7]]
+            output_to_csv = [binaryPath] + cfg_node_size + cfg_sys_calls_info + cfg_looping_times_info + cfg_return_info + cfg_block_instructions_info + radare2_max_complexity + radare2_complexity + angr_mc_cabe_complexity
+            csv_out.writerow(output_to_csv)
+
+
+if __name__ == '__main__':
+    write_to_csv(sys.argv[1], sys.argv[2], sys.argv[3], False)
--- feature_extraction/extractor.py
+++ feature_extraction/extractor.py
@@ -19,13 +19,14 @@ class FeatureExtractor(object):
         self._cfg_fast = None
         self._cfg_accurate = None
 
-    def __get_cfg__(self,use_accurate = False):
+    def __get_cfg__(self,use_accurate=False):
         if use_accurate:
             if self._cfg_accurate is None:
                 self._cfg_accurate = self._angr_project.analyses.CFGAccurate()
             return self._cfg_accurate
-        elif self._cfg_fast is None:
-            self._cfg_fast = self._angr_project.analyses.CFGFast()
+        elif not use_accurate:
+            if self._cfg_fast is None:
+                self._cfg_fast = self._angr_project.analyses.CFGFast()
             return self._cfg_fast
 
     def get_simple_complete_mccabe_complexity_angr(self, use_accurate=False):
@@ -35,7 +36,7 @@ class FeatureExtractor(object):
     def get_complex_complete_mccabe_complexity_angr(self, use_accurate=False):
         cfg = self.__get_cfg__(use_accurate)
         graph_for_tarjan_algorithm = {}
-        for n, nbrsdict in cfg.adjacency():
+        for n, nbrsdict in cfg.graph.adjacency():
             if len(list(nbrsdict)) >= 2:
                 graph_for_tarjan_algorithm[n] = list(nbrsdict)
         return cfg.graph.number_of_edges() - len(cfg.graph) + 2 * len(tarjan.tarjan(graph_for_tarjan_algorithm))
@@ -47,7 +48,8 @@ class FeatureExtractor(object):
         list = []
         for func_addr, functionObject in cfg.kb.functions.iteritems():
             cc = r2.cmdj("afCc@" + hex(func_addr))
-            list.append((functionObject.name, cc))
+            if cc is not None and cc >= 0:
+                list.append((functionObject.name, cc))
         ccList = [y for _, y in list]
         return {"max": max(ccList), "sum": sum(ccList), "list": list}
 
@@ -58,7 +60,7 @@ class FeatureExtractor(object):
         looping_times_info = {'sum': 0, 'max': 0}
         has_return_info = {'sum': 0}
         block_instructions_info = {'max': 0}
-        for nodeInGraph,info in cfg.graph.nodes.iteritems():
+        for nodeInGraph, info in cfg.graph.nodes.iteritems():
             # block sizes info
             sum_blocks_sizes_info["sum"] += nodeInGraph.size
             sum_blocks_sizes_info["max"] = max(sum_blocks_sizes_info["max"], nodeInGraph.size)
@@ -67,6 +69,7 @@ class FeatureExtractor(object):
             if nodeInGraph.is_syscall:
                 sum_sys_calls_info["sum"] += 1
                 sum_sys_calls_info["names"].append(nodeInGraph.syscall_name)
+
             # looping times info
             looping_times_info["sum"] += nodeInGraph.looping_times
             looping_times_info["max"] = max(looping_times_info["max"], nodeInGraph.looping_times)
@@ -83,12 +86,11 @@ class FeatureExtractor(object):
         s_analysis = self.radare2_cc_estimation(use_accurate_cfg)
         t_analysis = self.get_complex_complete_mccabe_complexity_angr(use_accurate_cfg)
         fourth_analysis = self.get_simple_complete_mccabe_complexity_angr(use_accurate_cfg)
-        return [f_analysis[0]["sum"], f_analysis[0]["max"], f_analysis[1]["sum"], f_analysis[1]["names"],
-                f_analysis[2]["sum"], f_analysis[2]["max"], f_analysis[3]["sum"], f_analysis[4]["max"], s_analysis,
+        return [f_analysis[0],  f_analysis[1], f_analysis[2], f_analysis[3], f_analysis[4], s_analysis,
                 t_analysis, fourth_analysis]
 
     @staticmethod
     def features_names_list():
-        return ["Cfg node size - sum", "Cfg node size - max", "system calls - count", "system calls - names",
-                "looping times - sum", "looping times - max", "Cfg node with return - count",
-                "Biggest Cfg node instruction count", "radare2", "complex McCabe", "simple McCabe"]
+        return ["Cfg Node Size", "System Calls",
+                "Looping Times", "Cfg Node With Return", "Biggest Cfg Node Instruction",
+                "Radare2", "Complex McCabe", "Simple McCabe"]
